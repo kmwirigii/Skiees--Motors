57d39a19b50d390845c7abae9cfb02a7bb462e02
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-03-26 14:32:03 +0300

    Added API call to fetch and render vehicle information

--- script.js
+++ script.js
@@ -9,4 +9,29 @@ document.addEventListener("DOMContentLoaded", () => {
     let carsData = [];
   
   let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+
+  fetch("http://localhost:3000/cars")
+    .then(response => response.json())
+    .then(data => {
+        carsData = data;
+        displayCars(carsData);
+    });
+
+function displayCars(cars) {
+    carContainer.innerHTML = "";
+    cars.forEach(car => {
+        const carCard = document.createElement("div");
+        carCard.classList.add("car-card");
+        carCard.innerHTML = `
+            <img src="${car.image}" alt="${car.name}">
+            <h3>${car.name}</h3>
+            <p>Price: ${car.price}</p>
+            <p>Type: ${car.type}</p>
+            <button class="favorite-btn" data-name="${car.name}">❤️ Add to Favorites</button>
+        `;
+        carContainer.appendChild(carCard);
+    });
+}
+
 });
